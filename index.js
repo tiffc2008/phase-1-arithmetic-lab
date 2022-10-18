7d4189b23a6d8dc3643ddf6913304f20cbe8f203
Tiffany Cheng <tiffanycheng.001@gmail.com>
2022-10-18 14:51:56 -0600

    commit tree 59d856914543232532a110a63638923a20bcb179

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.round(Math.random()*10);
+let num3 = 8;
+let num4 = 2;
+let mod = num3/num4;
+let max = Math.max(1,4,7,20);
